2f8126081ba463602b70026a30aaf98e7d9a5f0d
Rishabh Mehta <47055064+MehtaRishabh47@users.noreply.github.com>
2024-08-05 12:40:21 +0530

    stage changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/flexshark', // This should match your repository name
+  base: '/flexshark/', 
 });
